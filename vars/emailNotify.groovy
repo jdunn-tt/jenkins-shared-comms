923d9d6ed9e02f9102c4a10f2d033ca6e223a156
John Dunn <jdunn@timetrade.com>
2019-01-07 16:32:03 -0500

    cleanup of email notify

--- vars/emailNotify.groovy
+++ vars/emailNotify.groovy
@@ -8,12 +8,9 @@
     try {
         // Returns string containing recipients separated by whitespace
         def recipients = emailextrecipients([[$class: 'DevelopersRecipientProvider']])
-        //Testing
-        echo recipients
 
         if (recipients != null && !recipients.isEmpty()) {
             def recipientsArray = recipients.split(' ')
-            // timetradeRecipients = Arrays.asList(recipientsArray).contains(CORPORATE_DOMAIN)
             def timetradeRecipients = ""
 
             for (def recipient_index = 0; recipient_index < recipientsArray.length; recipient_index++) {
@@ -39,33 +36,10 @@
                 echo "WARNING: email notification was NOT sent: any of contributors emails doesn't belong to TimeTrade!"
             }
         }
-        // } else {
-        //     def recipient = getCommitAuthorsEmail()
-
-        //     if (recipient.endsWith(CORPORATE_DOMAIN)) {
-        //         emailext (
-        //             to: recipient,
-        //             subject: jobHeadline + "!",
-        //             body: """<p>${jobHeadline}:</p>
-        //                 <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a> to view the results.</p>
-        //                 <p>${errorMessage}</p>"""
-        //         )
-        //     } else {
-        //         echo "WARNING: email notification was NOT sent: commit author's email doesn't belong to TimeTrade!"
-        //     }
-        // }
     } catch(e) {
         echo "WARNING: sending email message FAILED: ${e.getMessage()}"
     }
  }
-
- /*
- * Retrieves the Git commit author's email for the given commit ID.
- *
- * @param gitCommitId String Git commit ID.
- *
- * @return String Git commit author's email.
- */
 
 def getCommitAuthorsEmail(gitCommitId = '') {
     if (gitCommitId == '') {
