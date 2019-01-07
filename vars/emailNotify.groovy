47c16e65c537fc6ad01cc19320bbd4cc2245f257
John Dunn <jdunn@timetrade.com>
2019-01-07 13:40:41 -0500

    debugging email notify

--- vars/emailNotify.groovy
+++ vars/emailNotify.groovy
@@ -26,7 +26,7 @@
 
         if (!timetradeRecipients.length() > 0 || getCommitAuthorsEmail.endsWith(CORPORATE_DOMAIN)) {
             emailext (
-                to: timetradeRecipients.isEmpty() ? getCommitAuthorsEmail() : timetradeRecipients,
+                to: timetradeRecipients.length() > 0 ? getCommitAuthorsEmail() : timetradeRecipients,
                 subject: jobHeadline + "!",
                 body: """<p>${jobHeadline}:</p>
                     <p>Check console output at <a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a> to view the results.</p>
