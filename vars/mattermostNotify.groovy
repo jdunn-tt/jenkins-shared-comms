d8365c621c880606473f765ae770a0fd7388bb33
John Dunn <jdunn@timetrade.com>
2019-01-09 09:59:23 -0500

    syntax error on mattermost test

--- vars/mattermostNotify.groovy
+++ vars/mattermostNotify.groovy
@@ -2,7 +2,7 @@ def call() {
 
     try {
         mattermostSend color: 'good', message: 'Message from Jenkins Pipeline', text: 'testing message from pipeline.'
-    } catch {
+    } catch(e) {
         echo "WARNING: sending HipChat message FAILED: ${e.getMessage()}"
     }
 
